ba93af122ab04427eff4cbbb3db2bb20b9b02e65
SerjiVutinss <SerjiVutinss@gmail.com>
2019-01-06 20:31:46 +0000

    Tidying up CosineSimilarityController
    Changed some variable names, removed unneccessary variables for cleaner code

--- src/ie/gmit/sw/CosineSimilarityController.java
+++ src/ie/gmit/sw/CosineSimilarityController.java
@@ -21,15 +21,21 @@ import ie.gmit.sw.cosine_distance.data_structures.MapBlock;
 import ie.gmit.sw.cosine_distance.data_structures.RunConfiguration;
 import ie.gmit.sw.cosine_distance.mapper.MapperController;
 
+/**
+ * Compares a query file to one or more subject files
+ * 
+ * @author Justin
+ *
+ */
 public class CosineSimilarityController {
 
+	// variables to hold futures of mapped files
 	private Future<MapBlock> queryFrequencyMap = null;
-	private int shingleSize;
-	@SuppressWarnings("unused")
-	private int queueCapacity = 50;
+	private BlockingQueue<Future<MapBlock>> subjectFrequencyMaps;
 
-	private volatile MapBlock queryMap;
-	private BlockingQueue<Future<MapBlock>> subjectFrequencyMaps = new ArrayBlockingQueue<>(queueCapacity);
+	// configuration variables
+	private int shingleSize;
+	private int queueCapacity = 50;
 
 	private int maxThreadCount = 0;
 	private String queryFilePath;
@@ -61,62 +67,56 @@ public class CosineSimilarityController {
 	public void start() throws InterruptedException, ExecutionException {
 
 		long startTime = System.nanoTime();
-
-		// input files/folder
-//		queryFilePath = Utils.getResourceDir() + "WarAndPeace-LeoTolstoy.txt";
-//		subjectFolderPath = Utils.getResourceDir() + "input_files/";
-
-		//
+		// instantiate data structures
+		subjectFrequencyMaps = new ArrayBlockingQueue<>(queueCapacity);
 		file_paths = new ArrayList<>();
-
 		subject_files = new HashMap<Integer, String>();
 
+		// build the list of files and the HashMap of filename hash, filename
+		this.buildFileList();
+
+		incrementThreadCount();
+		// Create and run a MapperController to map all files, maps will be stored in
+		// this instance's variables - queryFrequencyMap and subjectFrequencyMap
+		new MapperController(queryFilePath, file_paths, queryFrequencyMap, subjectFrequencyMaps, this, shingleSize,
+				queueCapacity).run();
+		incrementThreadCount();
+
+		ExecutorService executors = Executors.newCachedThreadPool(); // create an ExecutorService
+		// create and start a ComparerController for the mapped files - blocks until all
+		// have completed
+		executors.submit(new ComparerController(queryFrequencyMap, subjectFrequencyMaps)).get();
+		incrementThreadCount();
+		executors.shutdown(); // not really needed but added for completeness
+
+		incrementThreadCount();
+		long endTime = System.nanoTime();
+
+		// print out a summary
+		System.out.println("Max Threads used: " + maxThreadCount);
+		System.out.println("Total Duration: " + (double) ((double) (endTime - startTime) / 1000000000) + " seconds");
+		System.exit(0);
+	}
+
+	/**
+	 * Build a list of files and a HashMap for quick lookup of filenames
+	 */
+	private void buildFileList() {
 		try (Stream<Path> paths = Files.walk(Paths.get(subjectFolderPath))) {
 			paths.filter(Files::isRegularFile).forEach(file_paths::add);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		incrementThreadCount(Thread.activeCount());
-
 		for (Path p : file_paths) {
 			subject_files.put(p.toAbsolutePath().toString().hashCode(), p.toAbsolutePath().toString());
 		}
-
-		new MapperController(queryFilePath, file_paths, queryFrequencyMap, subjectFrequencyMaps, this, shingleSize,
-				queueCapacity).run();
-
-		incrementThreadCount(Thread.activeCount());
-
-		ExecutorService executors = Executors.newCachedThreadPool();
-		ComparerController cc = new ComparerController(queryFrequencyMap, subjectFrequencyMaps);
-		incrementThreadCount(Thread.activeCount());
-
-		Future<Boolean> result = executors.submit(cc);
-
-		boolean keepAlive = true;
-		while (keepAlive) {
-			try {
-				queryMap = queryFrequencyMap.get();
-				incrementThreadCount(Thread.activeCount());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			keepAlive = !result.isDone();
-			incrementThreadCount(Thread.activeCount());
-		}
-		executors.shutdown();
-//		executors.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
-		incrementThreadCount(Thread.activeCount());
-		if (executors.isShutdown()) {
-			long endTime = System.nanoTime();
-			System.out.println("Max Threads used: " + maxThreadCount);
-			System.out
-					.println("Total Duration: " + (double) ((double) (endTime - startTime) / 1000000000) + " seconds");
-		}
-		System.exit(0);
 	}
 
-	public void incrementThreadCount(int newCount) {
+	/**
+	 * Used for testing, try to find max number of threads used
+	 */
+	public void incrementThreadCount() {
+		int newCount = Thread.activeCount();
 		if (newCount > maxThreadCount) {
 			maxThreadCount = newCount;
 		}
